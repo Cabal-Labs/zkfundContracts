4e7f041c4d72d9c42be0f46a8b63f0decc01de3a
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2023-02-28 10:50:52 -0500

    testing

--- contracts/ValidateCharities.sol
+++ contracts/ValidateCharities.sol
@@ -2,12 +2,18 @@
 pragma solidity >0.7.0 <=0.8.18;
 import "./Counters.sol";
 
-contract ValidateCharities{
+contract ValidateCharities {
     using Counters for Counters.Counter;
     address public owner;
+    // keep a store of who is a validator
+    mapping (address => bool) public validators;
+    mapping (uint256 => CharitySnapshot) public charities;
+    Counters.Counter private _validatorCount;
+    Counters.Counter private _charityIds;
     constructor(){
         owner = msg.sender;
-        validators[owner] = true;
+        validators[msg.sender] = true;
+        _validatorCount.increment();
     }
     enum CharityStatus {
         Pending, Disapproved, Approved
@@ -31,43 +37,41 @@ contract ValidateCharities{
     // -------------------------------------------------
 
     // ========== Validator System ================
-    // keep a store of who is a validator
-    mapping (address => bool) public validators;
-    Counters.Counter private _validatorCount;
-    // have a way to require that only validators can do certain things (done)
+    // have a way to require that only validators can do certain things
+    function getValidatorCount() public view returns (uint256){
+        return _validatorCount.current();
+    }
     modifier onlyValidator() {
         require(validators[msg.sender] || msg.sender == owner, "Only Validators Can Do This Action");_;
     }
-
-    function addValidator(address newValidator) external onlyValidator{
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only Owner Can Do This Action");_;
+    }
+    function addValidator(address newValidator) external onlyOwner{
         _validatorCount.increment();
         validators[newValidator] = true;
     }
-    function removeValidator(address removingValidator) external onlyValidator{
+    function removeValidator(address removingValidator) external onlyOwner{
         _validatorCount.decrement();
         validators[removingValidator] = false;
     }
     // ===========================================
     // ============ Charity Voting System ========
-    Counters.Counter private _charityIds;
-    mapping (uint256 => CharitySnapshot) public charities;
-
     event CharityCreated(address charityAddress, string name, uint256 charityId);
     event CharityApproved(address charityAddress, string name, uint256 charityId);
     // maps a charity to a number of approvedVotes
-    mapping (address => uint256) private approveVotes;
+    mapping (uint256 => uint256) private approveVotes;
     // maps a charity to a nubmer of disapproveVotes
-    mapping (address => uint256) private disapproveVotes;
+    mapping (uint256 => uint256) private disapproveVotes;
     // tracks if a validator has voted for a specific charity or not
-    mapping (address => mapping(address => bool)) private validatorToCharity;
-    // ex: validatorToCharity[validatorAddress][charityAddress] = bool
-    // cant know what someone voted for, might want to change this going forward
-    modifier notVoted(address validator, address charityAddress) {
-        require(!validatorToCharity[validator][charityAddress], "Validator has already voted for this charity");
+    mapping (address => mapping(uint256 => bool)) private validatorToCharity;
+    // ex: validatorToCharity[validatorAddress][charityId] = bool
+
+    modifier notVoted(address validator, uint256 charityId) {
+        require(!validatorToCharity[validator][charityId], "Validator has already voted for this charity");
         _;
     }
-    // todo
-    function getCharites() public returns (CharitySnapshot[] memory){
+    function getCharities() public view returns (CharitySnapshot[] memory){
         // returns all charities
         CharitySnapshot[] memory _charities = new CharitySnapshot[](_charityIds.current());
         for (uint256 i = 0; i < _charityIds.current(); i++) {
@@ -75,21 +79,11 @@ contract ValidateCharities{
         }
         return _charities;
     }
-    // function getPendingCharities() public returns (CharitySnapshot[] memory){
-    //     // returns all pending charities
-    //     CharitySnapshot[] memory _charities = new CharitySnapshot[](_charityIds.current());
-    //     for (uint256 i = 0; i < _charityIds.current(); i++) {
-    //         if (charities[i].status == charityStatus.Pending){
-    //             _charities[i] = charities[i];
-    //         }
-    //     }
-    //     return _charities;
-    // }
-    function getCharityStatus(uint256 charityId) public returns (CharityStatus) {
+    function getCharityStatus(uint256 charityId) public view returns (CharityStatus) {
         // returns the status of a charity
         return charities[charityId].status;
     }
-    function inititlizeCharity (address walletAddress, string memory name,string memory charityId, bool ownsWallet) public onlyValidator{
+    function initCharity (address walletAddress, string memory name, bool ownsWallet) public onlyValidator{
         _charityIds.increment();
 
         uint256 newItemId = _charityIds.current();
@@ -103,26 +97,24 @@ contract ValidateCharities{
         charities[newItemId] = charity;
         emit CharityCreated(walletAddress, name, newItemId);
     }
-
-    function voteApprove(address charityAddress) external onlyValidator notVoted(msg.sender, charityAddress){
-        validatorToCharity[msg.sender][charityAddress] = true;
-        approveVotes[charityAddress]++;
-        isCharityApproved(charityAddress);
+    function voteApprove(uint256 charityId) public onlyValidator notVoted(msg.sender, charityId){
+        validatorToCharity[msg.sender][charityId] = true;
+        approveVotes[charityId]++;
+        approveCharity(charityId);
     }
 
-    function voteDisapprove(address charityAddress) external onlyValidator notVoted(msg.sender, charityAddress){
-        validatorToCharity[msg.sender][charityAddress] = true;
-        disapproveVotes[charityAddress]++;
-        isCharityApproved(charityAddress);
+    function voteDisapprove(uint256 charityId) external onlyValidator notVoted(msg.sender, charityId){
+        validatorToCharity[msg.sender][charityId] = true;
+        disapproveVotes[charityId]++;
+        approveCharity(charityId);
     }
-
-    function isCharityApproved(address charityAddress) external view returns (bool){
-        uint256 minimumVotes = _validatorCount * 100 / 66; // require at least 2/3 turnout
-        uint256 totalVotes = approveVotes[charityAddress] + disapproveVotes[charityAddress];
-        bool result = (totalVotes > minimumVotes && approveVotes[charityAddress] * 100 / totalVotes >= 75);// 75% approval
+    function approveCharity(uint256 charityId) internal returns (bool){
+        uint256 minimumVotes = _validatorCount.current() * 100 / 66; // require at least 2/3 turnout
+        uint256 totalVotes = approveVotes[charityId] + disapproveVotes[charityId];
+        bool result = (totalVotes > minimumVotes && approveVotes[charityId] * 100 / totalVotes >= 75);// 75% approval
         if (result){
-            charities[charityAddress].status = CharityStatus.Approved;
-            emit CharityApproved(charityAddress, charities[charityAddress].name, charities[charityAddress].charityId);
+            charities[charityId].status = CharityStatus.Approved;
+            emit CharityApproved(charities[charityId].walletAddress, charities[charityId].name, charityId);
         }
         return result;
     }
--- test/ValidateCharities.ts
+++ test/ValidateCharities.ts
@@ -8,12 +8,111 @@ describe("ValidateCharities", function () {
 			"ValidateCharities"
 		);
 		const validateCharities = await ValidateCharities.deploy();
-		return validateCharities;
+		let owner = await validateCharities.owner();
+		// create a list of 10 charities
+		const initialCharities = [
+			{
+				name: "Charity 1",
+				walletAddress: `0x${Math.floor(Math.random() * 100)}`,
+				//?^ is this the right way to generate a random address?
+				hasWallet: true,
+			},
+			{
+				name: "Charity 2",
+				walletAddress: "",
+				hasWallet: false,
+			},
+			{
+				name: "Charity 3",
+				walletAddress: "",
+				hasWallet: false,
+			},
+			{
+				name: "Charity 4",
+				walletAddress: "",
+				hasWallet: false,
+			},
+			{
+				name: "Charity 5",
+				walletAddress: "",
+				hasWallet: false,
+			},
+			{
+				name: "Charity 6",
+				walletAddress: "",
+				hasWallet: false,
+			},
+			{
+				name: "Charity 7",
+				walletAddress: "",
+				hasWallet: false,
+			},
+			{
+				name: "Charity 8",
+				walletAddress: "",
+				hasWallet: false,
+			},
+			{
+				name: "Charity 9",
+				walletAddress: "",
+				hasWallet: false,
+			},
+			{
+				name: "Charity 10",
+				walletAddress: "",
+				hasWallet: false,
+			},
+		];
+		// initialize those charities
+		for (let i = 0; i < initialCharities.length; i++) {
+			await validateCharities.initCharity(
+				initialCharities[i].name,
+				initialCharities[i].walletAddress,
+				initialCharities[i].hasWallet
+			);
+		}
+
+		return { validateCharities, owner };
 	}
 	describe("Deployment", function () {
-		it("should return an empty list of charities", async function () {
-			const validateCharities = await deployValidateCharitiesFixture();
-			expect(await validateCharities.getCharities()).to.be.empty;
+		it("should return 10 charities", async function () {
+			const { validateCharities } = await deployValidateCharitiesFixture();
+			expect(await validateCharities.getCharities()).length.to.equal(10);
 		});
+		it("should have the owner as the only validator", async function () {
+			const { validateCharities, owner } =
+				await deployValidateCharitiesFixture();
+			console.log(owner);
+			expect(await validateCharities.getValidatorCount()).to.equal(1);
+			expect(await validateCharities.validators.call(owner, "")).to.equal(true);
+		});
+	});
+	describe("Validator Creation", function () {
+		it("should allow a validator to create a new validator", async function () {});
+		it("should prevent a non-validator from creating a validator", async function () {});
+		it("should prevent a validator from being created twice", async function () {});
+	});
+	describe("Charity Creation", function () {
+		it("should allow the any validator to create a charity", async function () {});
+		it("should prevent a non-validator from creating a charity", async function () {});
+		it("should prevent two charities with the same name from being created", async function () {});
+		it("should prevent two charities with the same address from being created", async function () {});
+	});
+	describe("Voting", function () {
+		it("should allow a validator to vote", async function () {});
+		it("should prevent a non-validator from voting", async function () {});
+		it("should prevent a validator from voting twice", async function () {});
+		it("should prevent a validator from voting for a charity that doesn't exist", async function () {});
+		it("should prevent a validator from voting for a charity that has already been approved", async function () {});
+		it("should prevent a validator from voting for a charity that has already been rejected", async function () {});
+		it("should be able to approve a charity", async function () {});
+		it("should be able to reject a charity", async function () {});
+		it("should prevent a charity from being approved if it has been rejected", async function () {});
+		it("should prevent a charity from being rejected if it has been approved", async function () {});
+		it("should prevent a charity from being approved if it has already been approved", async function () {});
+		it("should prevent a charity from being rejected if it has already been rejected", async function () {});
+	});
+	describe("Approving/Disapproving Charities", function () {
+		it("should allow a charity with 75% approval and 66% validator turnout to be approved", async function () {});
 	});
 });
