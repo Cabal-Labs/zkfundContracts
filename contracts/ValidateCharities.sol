cc6e898fdbbd472886be85feb7c8ac67c2cead6d
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2023-02-28 16:34:00 -0500

    a

--- contracts/ValidateCharities.sol
+++ contracts/ValidateCharities.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >0.7.0 <=0.8.18;
-import "./Counters.sol";
+import "./Counters.sol";  
 
 contract ValidateCharities {
     using Counters for Counters.Counter;
